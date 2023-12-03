e5c5b4187ce466148172994c322a78521bb7bfc6
Ego Nwaekpe <glorynwaekpe@gmail.com>
2023-12-03 12:02:09 -0800

    added tailwind styling

--- src/app/memory_card/memory_card.jsx
+++ src/app/memory_card/memory_card.jsx
@@ -1,14 +1,15 @@
 "use client";
 import Image from "next/image";
 import React, { useState, useEffect } from "react";
+import "./memory_card.css";
 
 const cardSet = [
-  { image: "/egusi.png" },
-  { image: "/fufu.png" },
-  { image: "/garri.png" },
-  { image: "/jollof_rice.png" },
-  { image: "/nkwobi.png" },
-  { image: "/palm_wine.png" },
+  { image: "/camera.svg" },
+  { image: "/card.svg" },
+  { image: "/camera.svg" },
+  { image: "/card.svg" },
+  { image: "/camera.svg" },
+  { image: "/card.svg" },
 ];
 
 const Memory_card = () => {
@@ -30,11 +31,11 @@ const Memory_card = () => {
   //   }, [card]);
 
   return (
-    <>
+    <div className="bg-neutral-700">
       <div>Memory Card</div>
       <button
         onClick={shuffleCard}
-        className=" w-40 h-12   bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white font-bold p rounded-lg "
+        className="w-20 h-10  bg-[#8AA4E9] rounded-xl cursor-pointer"
       ></button>
 
       {card.map((cardItem) => (
@@ -46,20 +47,22 @@ const Memory_card = () => {
                 height={100}
                 src={cardItem.image}
                 alt="Food Image"
+                className="border  "
               />
             </div>
             <div>
               <Image
                 width={100}
                 height={100}
-                src="/card_back.png"
-                alt="Card Back"
+                src="/card.svg"
+                alt="Food Image"
+                className="border  "
               />
             </div>
           </div>
         </div>
       ))}
-    </>
+    </div>
   );
 };
 
